0ec8436dfaf49a911c805d4f9d67bea77e0b5470
vignesh <64148081+poppyseed64@users.noreply.github.com>
2020-08-13 23:40:37 +0530

    second commit added signup and login

---
+++ websitetin-master/app.js
@@ -0,0 +1,17 @@
+function contact(){
+    window.scroll(0,2500);
+
+}
+function redirect()
+{
+var url = "./signup.html";
+window.location.href=url;
+}
+function homeredirect(){
+    var url = "./index.html";
+ window.location.href=url;
+}
+function loginredirect(){
+    var url = "./login.html";
+ window.location.href=url;
+}
